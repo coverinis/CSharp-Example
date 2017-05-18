85c08998d8126a90f0722ec5f248705852ecb462
Shawn Coverini <coverinis@gmail.com>
2017-05-18 16:57:38 -0400

    Change properties

--- C# Example/Properties/AssemblyInfo.cs
+++ C# Example/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("scoverini5334-WaMP-Final")]
+[assembly: AssemblyTitle("SharpDraw")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("scoverini5334-WaMP-Final")]
+[assembly: AssemblyProduct("SharpDraw")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
